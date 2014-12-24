b2198b5f0ff400bdbd6f7b00921ea63a96403e13
Christophe Cluizel <christophe.cluizel@gmail.com>
2014-12-24 17:54:40 +0100

    Addition of the use of tic-toc.

--- src/challenge/scala/2.teads.scala
+++ src/challenge/scala/2.teads.scala
@@ -2,8 +2,96 @@ import math._
 import scala.util._
 import Array._
 import scala.collection.mutable.ArrayBuffer
+/**
+ * TicToc allows for easy benchmarking of different parts in source code.
+ * Just inherit the TicToc trait, then start a time measurement with tic,
+ * and stop it using toc.
+ * As of now tic-tocs can be nested but can not overlap, as the underlying
+ * data structure is a stack: a toc always terminates the last tic.
+ * The reason for this decision was that the TicToc trait and its methods
+ * should be as lightweight as possible in order not to falsify the measured
+ * run times.
+ * @author fgysin
+ */
+trait TicToc {
+  import scala.collection.mutable.LinkedList
+  import scala.collection.mutable.Stack
+  import java.io._
 
-class Graph[X](nbNodes: Int) {
+  var timeCounter: Int = 0
+  var tics: Stack[Long] = Stack()
+  var times: LinkedList[(String, Long)] = LinkedList()
+
+  /**
+   * Starts a time measurement.
+   */
+  def tic() {
+    tics = tics.push(System.currentTimeMillis())
+  }
+
+  /**
+   * Stops the last started time measurement.
+   * @param descr a description of the last measurement period
+   */
+  def toc(descr: String) {
+    var t = System.currentTimeMillis() - tics.pop()
+    if (null != descr && !"".equals(descr))
+      times = times :+ (descr, t)
+    else {
+      times = times :+ ("time" + timeCounter, t)
+      timeCounter += 1
+    }
+  }
+
+  /**
+   * Write the logged times to a file. There is some formatting sugar contained
+   * that checks if a file with this name already exists. If yes, the run times
+   * are added at the bottom of the existing file (this is useful if you repeat
+   * the same measurement multiple times), else the file is created.
+   * @param path The path of the file to write the times log.
+   */
+  def writeTimesLog(path: String) {
+    var linesToPrint = outLines
+    try {
+      // Check if the file was alreay created, if yes only
+      // append the timings not the timing information.
+      val lr = new LineNumberReader(new FileReader(path))
+      val read = lr.readLine()
+      if (null != read && read.startsWith("Timings")) {
+        linesToPrint = linesToPrint.drop(2)
+      }
+      lr.close()
+    } catch {
+      case _ =>
+    }
+    // Write the timings and or timing information.
+    val fw = new FileWriter(path, true);
+    linesToPrint.foreach { l => fw.write(l + "\n"); }
+    fw.close()
+  }
+
+  /**
+   * Print the logged times.
+   */
+  def printTimesLog() {
+    outLines.foreach { l => println(l); }
+  }
+
+  /**
+   * String output of the current times and their respective descriptions.
+   * @return
+   */
+  def outLines(): List[String] = {
+    var list = List[String]()
+    list ::= ("Timings of " + this.getClass().toString())
+    list ::= "description:" + times.map(x => "\t" + x._1).reduce(_ + _)
+    list ::= "times(ms):" + times.map(x => "\t" + x._2).reduce(_ + _)
+    list.reverse
+  }
+
+}
+
+class Graph[X](nbNodes: Int) extends TicToc{
     var adjacence: Map[Int, ArrayBuffer[Int]] = Map()
 
     def addNode(key: Int, node: X) = {
@@ -76,7 +164,7 @@ class Graph[X](nbNodes: Int) {
     }
 }
 
-object Solution {
+object Solution extends TicToc {
     def main(args: Array[String]) {
         var nbNodes = readInt
         nbNodes += 1
@@ -85,6 +173,7 @@ object Solution {
         var radius = 0
         var eccentricityMin = 1000000
 
+        tic
         for(i <- 0 until nbNodes - 1){
             val Array(key1, key2) = for(i <- readLine split " ") yield i.toInt
             if(!graph.nodePresent(key1)) graph.addNode(key1, key1)
@@ -92,11 +181,16 @@ object Solution {
             graph.addEdge(key1, key2, 1)
             graph.addEdge(key2, key1, 1)
         }
+        toc("Construction of the graph")
         // graph.display
+
+        tic
         graph.adjacence.keys.foreach {i =>
             eccentricity = graph.calculateEccentricityOf(i)
             if(eccentricity < eccentricityMin) eccentricityMin = eccentricity
         }
+        toc("Calculation of eccentricity")
+        Console.err.println(outLines)
 
         // Console.err.println("key : " + i + " -> eccentricity : " + eccentricity(i))
         // Console.err.println("eccentricityMin " + eccentricityMin)
