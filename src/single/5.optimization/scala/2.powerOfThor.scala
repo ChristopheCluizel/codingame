eec9f6b2f95a6ddb560578611f6a3dac3e5ccd99
Christophe Cluizel <christophe.cluizel@gmail.com>
2015-04-28 23:03:48 +0200

    perf(optimization): remove the main bloc

--- src/single/5.optimization/scala/2.powerOfThor.scala
+++ src/single/5.optimization/scala/2.powerOfThor.scala
@@ -1,11 +1,9 @@
-object Player {
-  def main(a: Array[String]) {
-    var Array(w, x, y, z) = for(i <- readLine split " ") yield i.toInt
-    while(true) {
-        var o = ""
-        if(z < x) {o="S"; z+=1} else if(z > x) {o="N"; z-=1}
-        if(y < w) {o+="E"; y+=1} else if(y > w) {o+="W"; y-=1}
-        println(o)
-    }
+object Player extends App {
+  var Array(w, x, y, z) = for(i <- readLine split " ") yield i.toInt
+  while(true) {
+    var o = ""
+    if(z < x) {o="S"; z+=1} else if(z > x) {o="N"; z-=1}
+    if(y < w) {o+="E"; y+=1} else if(y > w) {o+="W"; y-=1}
+    println(o)
   }
 }
