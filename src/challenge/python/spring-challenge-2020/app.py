21e0c30e960eed66b023c207ee504829d026fb60
Christophe Cluizel <christophe.cluizel@gmail.com>
2020-05-08 11:00:58 +0200

    final version to level-up to bronze league

--- src/challenge/python/spring-challenge-2020/app.py
+++ src/challenge/python/spring-challenge-2020/app.py
@@ -2,6 +2,7 @@ import sys
 import math
 import json
 import uuid
+import random
 
 DEBUG = False
 
@@ -36,9 +37,23 @@ class Pellet:
 
 
 class Pac:
-    def __init__(self, id, position):
+    def __init__(self, id):
         self.id = id
-        self.position = position
+        self.positions = []
+        self.current_position = None
+
+    def __str__(self):
+        return f"id: {self.id}, current_position: {self.current_position}, all_positions: {','.join([str(position) for position in self.positions])}"
+
+    def move(self, new_position):
+        self.current_position = new_position
+        self.positions.append(new_position)
+
+    def is_blocked(self):
+        if len(self.positions) < 2:
+            return False
+        else:
+            return self.current_position.is_equal(self.positions[-2])
 
 
 class Map:
@@ -78,19 +93,31 @@ class Game:
         self.map = map
         self.my_pacs = []
         self.pellets = []
+        self.orders = []
 
     def __str__(self):
         return ""
 
-    def get_best_pellet(self):
-        sorted_pellets = sorted(
-            self.pellets, key=lambda pellet: pellet.score, reverse=True
+    def get_best_pellets(self):
+        sorted_pellets = sorted(self.pellets, key=lambda pellet: pellet.score)
+
+        return sorted_pellets
+
+    def get_closest_pellets(self, current_position):
+        pellets_distances = [
+            (pellet, current_position.distance_with(pellet.position))
+            for pellet in self.pellets
+        ]
+        sorted_pellets_distances = sorted(
+            pellets_distances, key=lambda tuple: tuple[1], reverse=True
         )
 
-        if len(sorted_pellets) > 0:
-            return sorted_pellets[0]
-        else:
-            None
+        return [tuple[0] for tuple in sorted_pellets_distances]
+
+    def get_random_pellests(self):
+        temp = [pellet for pellet in self.pellets]
+        random.shuffle(temp)
+        return temp
 
 
 if __name__ == "__main__":
@@ -102,15 +129,9 @@ if __name__ == "__main__":
     map = Map(width, height)
     game = Game(map)
 
-    # for row_index in range(7):
-    #     for column_index, tile in enumerate(input()):
-    #         map.add_tile(row_index, column_index, tile)
-
     for row_index in range(height):
         for column_index, tile in enumerate(input()):
             map.add_tile(row_index, column_index, tile)
-
-    # print_debug(map)
 
     # game loop
     while True:
@@ -141,16 +162,38 @@ if __name__ == "__main__":
             ability_cooldown = int(ability_cooldown)
 
             if mine:
-                game.my_pacs.append(Pac(pac_id, Position(x, y)))
+                temp_pacs = [pac for pac in game.my_pacs if pac.id == pac_id]
+
+                if len(temp_pacs) == 0:
+                    # print_debug(f"initialise pac {pac_id}")
+                    game.my_pacs.append(Pac(pac_id))
+                    game.my_pacs[-1].move(Position(x, y))
+                else:
+                    # print_debug(f"update pac {pac_id}")
+                    my_pac = temp_pacs[0]
+                    my_pac.move(Position(x, y))
+
+        # for pac in game.my_pacs:
+        #     print_debug(pac)
 
         visible_pellet_count = int(input())  # all pellets in sight
+
         game.pellets = []
         for i in range(visible_pellet_count):
             # value: amount of points this pellet is worth
             x, y, value = [int(j) for j in input().split()]
             game.pellets.append(Pellet(Position(x, y), value))
 
-        best_pellet = game.get_best_pellet()
+        orders = []
 
-        # MOVE <pacId> <x> <y>
-        print(f"MOVE 0 {best_pellet.position.x} {best_pellet.position.y}")
+        for pac in game.my_pacs:
+            best_pellets = game.get_closest_pellets(pac.current_position)
+            best_pellet = best_pellets.pop()
+            if pac.is_blocked():
+                best_pellet = game.get_random_pellests()[0]
+            orders.append(
+                f"MOVE {pac.id} {best_pellet.position.x} {best_pellet.position.y}"
+            )
+
+        string_orders = "|".join(orders)
+        print(string_orders)
